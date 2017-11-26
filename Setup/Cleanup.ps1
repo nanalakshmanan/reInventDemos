1399a2a0dcc2e7350b4d1cfa7cd59e1f3eee7f6a
Nana Lakshmanan <nanalakshmanan@gmail.com>
2017-11-25 19:41:52 -0800

    Cloud formation templates for creating Windows Instances (#2)
    * Cloudformation template and setup/cleanup updates for creating Windows Instances with RDP access
    
    * Tagging Windows and Linux Instances upon creation

--- Setup/Cleanup.ps1
+++ Setup/Cleanup.ps1
@@ -3,13 +3,30 @@ param(
 )
 $EmailLambdaStack = 'EmailLambdaStack'
 $LinuxInstanceStack = 'LinuxInstanceStack'
+$WindowsInstanceStack = 'WindowsInstanceStack'
 $InstallApacheDocName = 'Nana-InstallApache'
 $BounceHostName = 'Nana-BounceHostRunbook'
 
-$StackName, $EmailLambdaStack, $LinuxInstanceStack | % {
+$AllStacks = @($EmailLambdaStack, $LinuxInstanceStack, $WindowsInstanceStack)
+function Wait-Stack
+{
+	param(
+		[string]
+		$StackName
+	)
+	while(Test-CFNStack -StackName $StackName){
+		Write-Verbose "Waiting for Stack $StackName to be deleted"
+		Start-Sleep -Seconds 3
+	}
+}
+$AllStacks | % {
 	if (Test-CFNStack -StackName $_){
 		Remove-CFNStack -StackName $_ -Force
 	}
+}
+
+$AllStacks | % {
+	Wait-Stack -StackName $_
 }
 
 Remove-SSMDocument -Name $InstallApacheDocName -Force
